1e52239be78e0e04b4d0ade16d4d3cd3a69f839e
Rey Vincent Babilonia <rey.babilonia@blinkpay.co.nz>
2025-10-13 16:31:36 +1300

    Bumped dependencies.

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,27 @@
-module.exports = {
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
     preset: 'ts-jest',
     testEnvironment: 'node',
-    testMatch: ['**/?(*.)+(spec|test).ts?(x)', '**/integrationTests/**/*.[jt]s?(x)']
+    testMatch: ['**/?(*.)+(spec|test).ts?(x)', '**/integrationTests/**/*.[jt]s?(x)'],
+
+    // 1) TS is compiled by ts-jest in ESM mode
+    transform: {
+        '^.+\\.(ts|tsx)$': ['ts-jest', { useESM: true, tsconfig: 'tsconfig.json' }],
+        // 2) JS (including node_modules/uuid) is compiled by babel-jest
+        '^.+\\.(js|jsx)$': 'babel-jest',
+    },
+
+    // 3) Allow transforming ESM deps in node_modules (uuid)
+    transformIgnorePatterns: ['/node_modules/(?!(uuid)/)'],
+
+    // 4) Treat TS as ESM in Jest’s runtime
+    extensionsToTreatAsEsm: ['.ts'],
+
+    // 5) Help Jest resolve ESM “.js” extension emitted by TS in relative imports
+    moduleNameMapper: {
+        '^(\\.{1,2}/.*)\\.js$': '$1',
+    },
 };
-  
+
+export default config;
